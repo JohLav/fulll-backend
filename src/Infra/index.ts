5e13de3b893cedffb73ce892f5df49f42834ea75
Johana Lavigne <johana.lavigne@gmail.com>
2025-05-04 14:56:51 +0200

    feat: prisma setup

---
+++ src/Infra/index.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from "../../generated/prisma/index";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const allFleets = await prisma.fleet.findMany();
+  console.log("All fleets:", allFleets);
+  const createFleet = await prisma.fleet.create({
+    data: {
+      id: "1",
+      userId: "1",
+    },
+  });
+  const allFleetsAfterCreate = await prisma.fleet.findMany();
+  console.log("All fleets - after create:", allFleetsAfterCreate);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });

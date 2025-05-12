63f326969a927c65b015f62d9c61864f1187ccbe
Johana Lavigne <johana.lavigne@gmail.com>
2025-05-12 22:05:57 +0200

    refacto: more readable tests

--- tests/FizzBuzz/fizzBuzz.test.ts
+++ tests/FizzBuzz/fizzBuzz.test.ts
@@ -1,36 +1,39 @@
-import { expect, test } from "vitest";
-import { BUZZ, FIZZ, FIZZBUZZ, fizzBuzz } from "./fizzBuzz.js";
+import { describe, expect, it } from "vitest";
+import { fizzBuzz } from "./fizzBuzz.js";
 
-test.each([
-  [1, "1"],
-  [2, "2"],
-  [7, "7"],
-  [0, FIZZBUZZ],
-  [-3, FIZZ],
-  [-5, BUZZ],
-  [-15, FIZZBUZZ],
-])("should return %s with input %i", (input, expected) => {
-  expect(fizzBuzz(input)).toBe(expected);
-});
+const FIZZ = "Fizz";
+const BUZZ = "Buzz";
+const FIZZBUZZ = "FizzBuzz";
 
-test.each([[3], [6], [9], [-3]])(
-  'should return "Fizz" with input %i',
-  (input) => {
+describe("fizzBuzz", () => {
+  it.each([
+    [1, "1"],
+    [2, "2"],
+    [7, "7"],
+  ])("should return %s with input %i", (input, expected) => {
+    expect(fizzBuzz(input)).toBe(expected);
+  });
+
+  it.each([
+    [3, FIZZ],
+    [6, FIZZ],
+    [9, FIZZ],
+  ])('should return "Fizz" with input %i', (input) => {
     expect(fizzBuzz(input)).toBe(FIZZ);
-  },
-);
+  });
 
-test.each([[5], [10], [-5]])('should return "Buzz" for input %i', (input) => {
-  expect(fizzBuzz(input)).toBe(BUZZ);
-});
+  it.each([
+    [5, BUZZ],
+    [10, BUZZ],
+  ])('should return "Buzz" for input %i', (input) => {
+    expect(fizzBuzz(input)).toBe(BUZZ);
+  });
 
-test.each([[15], [30], [-15]])(
-  'should return "FizzBuzz" for input %i',
-  (input) => {
+  it.each([
+    [15, FIZZBUZZ],
+    [30, FIZZBUZZ],
+    [3000, FIZZBUZZ],
+  ])('should return "FizzBuzz" for input %i', (input) => {
     expect(fizzBuzz(input)).toBe("FizzBuzz");
-  },
-);
-
-test("should handle large multiples correctly", () => {
-  expect(fizzBuzz(3000)).toBe("FizzBuzz");
+  });
 });

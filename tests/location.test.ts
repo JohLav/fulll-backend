7782acd2a845858bd65ada77762465f4cd59f49a
Johana Lavigne <johana.lavigne@gmail.com>
2025-05-13 09:44:58 +0200

    feat: it and removed describe

--- tests/location.test.ts
+++ tests/location.test.ts
@@ -1,12 +1,10 @@
-import { describe, expect, test } from "vitest";
+import { describe, expect, it } from "vitest";
 import { Location } from "../src/Domain/Models/Location.js";
 
 describe("Location", () => {
-  describe("create", () => {
-    test("should create a vehicle", function () {
-      const location = Location.create(48.8566, 2.3522);
+  it("should create a vehicle", function () {
+    const location = Location.create(48.8566, 2.3522);
 
-      expect(location).to.be.instanceOf(Location);
-    });
+    expect(location).to.be.instanceOf(Location);
   });
 });

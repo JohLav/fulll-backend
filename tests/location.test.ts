0d7e89c2bb17dd65d03f46e28a5bec3c592be77c
Johana Lavigne <johana.lavigne@gmail.com>
2025-05-13 13:46:06 +0200

    refacto: location's test

--- tests/location.test.ts
+++ tests/location.test.ts
@@ -2,18 +2,22 @@ import { describe, expect, it } from "vitest";
 import { Location } from "../src/Domain/Models/Location.js";
 
 describe("Location", () => {
-  it("should create a location", (): void => {
+  it("should create a location without altitude", (): void => {
     const location = Location.create(48.8566, 2.3522);
     expect(location).to.be.instanceOf(Location);
   });
 
-  it("should be equal to another location", (): void => {
-    const anotherLocation = Location.create(48.8566, 2.3522);
-    const isSameLocation = Location.create(48.8566, 2.3522).equals(
+  it("should create a location with altitude", (): void => {
+    const location = Location.create(48.8566, 2.3522, 100);
+    expect(location).to.be.instanceOf(Location);
+  });
+
+  it("should be equal to another location's latitude, longitude, altitude", (): void => {
+    const anotherLocation = Location.create(48.8566, 2.3522, 100);
+    const isSameLocation = Location.create(48.8566, 2.3522, 100).equals(
       anotherLocation,
     );
 
     expect(isSameLocation).toBeTruthy();
   });
-  // TODO: add test to verify altitude equality
 });
